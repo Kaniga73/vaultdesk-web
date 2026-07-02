5cfc586a4e18e22a158f7057e32d629064454ba7
Kaniga73 <prajalks@gmail.com>
2026-07-02 15:26:32 +0530

    commit changes

--- server/server.js
+++ server/server.js
@@ -1,27 +1,30 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const { protect } = require('./middleware/authMiddleware');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const { protect } = require('./middleware/authMiddleware');
 
-
-
-
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(express.json());
 
 
 connectDB();
 
-// Routes
-// Test protected route
+
+app.use('/api/auth', authRoutes);
+
+
 app.get('/api/protected', protect, (req, res) => {
   res.json({ message: `Hello ${req.user.name}, you are authorized!` });
 });
 
-app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => {
   res.send('VaultDesk backend is running');
 });
